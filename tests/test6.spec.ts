389a5b9fbc00a3f22923d377f892b0efc286be65
nguyentheduy2004 <duynguyenthe195@gmail.com>
2025-08-26 10:01:53 +0700

    thêm test.describe, test hook, test.step

--- tests/test6.spec.ts
+++ tests/test6.spec.ts
@@ -1,22 +1,35 @@
 import { test, expect } from '@playwright/test';
-test('Register Page ', async ({ page }) => {
-    await page.goto('https://material.playwrightvn.com/');
-    await page.waitForTimeout(500);
-    await page.getByText('Bài học 3: Todo page').click();
-    await page.waitForTimeout(500);
-    for (let i = 1; i <= 10; i++) {
+
+test.describe('Bài học 3: Todo page', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await test.step('Mở trang Todo page', async () => {
+      await page.goto('https://material.playwrightvn.com/');
+      await page.getByText('Bài học 3: Todo page').click();
+    });
+  });
+
+  test('Thêm 100 task và xoá các task lẻ', async ({ page }) => {
+
+    await test.step('Thêm 100 task', async () => {
+      for (let i = 1; i <= 100; i++) {
         await page.fill('//*[@id="new-task"]', `${i}`);
         await page.locator('//*[@id="add-task"]').click();
-    
-  }
-    for (let i = 1; i <= 10; i++) {
-        if (i % 2 != 0) {
-            page.once('dialog', async (dialog) => {
-            await dialog.accept();
-            });
-        await page.locator(`//li[.//span[text()="${i}"]]//button[text()="Delete"]`).click();
-        }
-}
+      }
+    });
 
-    
+    await test.step('Xoá các task có số lẻ', async () => {
+      for (let i = 1; i <= 100; i++) {
+        if (i % 2 !== 0) {
+          page.once('dialog', async (dialog) => {
+            await dialog.accept();
+          });
+          await page.locator(`//li[.//span[text()="${i}"]]//button[text()="Delete"]`).click();
+        }
+      }
+    });
+
+
+  });
+
 });
